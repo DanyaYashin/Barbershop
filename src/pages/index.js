021af730ed4bf8336d58d7e0bf2afc9fd7925c78
DanyaYashin <yashindan2000@gmail.com>
2020-10-09 15:49:44 +0400

    add usiki

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
+import styles from "./index.module.css"
+
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>usiki</div>
+
+  )
 }
